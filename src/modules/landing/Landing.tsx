87ef32ac37e64239b4f034692971944b9ddf74bd
blackparadise0407 <blackparadise0407@gmail.com>
2021-12-23 01:06:12 +0700

    add animation for landing page

--- src/modules/landing/Landing.tsx
+++ src/modules/landing/Landing.tsx
@@ -4,24 +4,32 @@ import { Link } from 'react-router-dom';
 
 export default function Landing() {
     return (
-        <div className="h-screen flex flex-col">
-            <nav className="flex py-8 bg-white px-2 sm:px-10 md:px-20 lg:px-36">
-                <div className="font-bold">Secure storage</div>
-                <FlexGrow />
-                <ul className="flex space-x-10">
-                    <li className="hover:text-gray-400 transition-colors cursor-pointer">
-                        Home
-                    </li>
-                    <li className="hover:text-gray-400 transition-colors cursor-pointer">
-                        About us
-                    </li>
-                    <li className="hover:text-gray-400 transition-colors cursor-pointer">
-                        Contact us
-                    </li>
-                </ul>
-            </nav>
-            <div className="flex flex-col justify-center h-full bg-indigo-200 px-2 sm:px-10 md:px-20 lg:px-36">
-                <div className="max-w-xl space-y-4">
+        <div className="h-screen flex flex-col bg-indigo-200">
+            <motion.div
+                animate={{ y: [-200, 0], opacity: [0, 1] }}
+                transition={{ duration: 1 }}
+            >
+                <nav className="flex bg-white items-center py-6 px-2 sm:px-10 md:px-20 lg:px-36">
+                    <div className="font-bold">Secure storage</div>
+                    <FlexGrow />
+                    <ul className="flex items-center space-x-10">
+                        <li className="hover:text-gray-400 transition-colors cursor-pointer">
+                            Home
+                        </li>
+                        <li className="hover:text-gray-400 transition-colors cursor-pointer">
+                            About us
+                        </li>
+                        <li className="hover:text-gray-400 transition-colors cursor-pointer">
+                            Contact us
+                        </li>
+                        <li className="h-10 hover:text-gray-400 transition-colors cursor-pointer">
+                            <Button type="primary">Sign in</Button>
+                        </li>
+                    </ul>
+                </nav>
+            </motion.div>
+            <div className="flex flex-col justify-center h-full  px-2 sm:px-10 md:px-20 lg:px-36">
+                <div className="max-w-xl space-y-5">
                     <motion.div
                         animate={{ x: [-500, 0], opacity: [0, 1] }}
                         transition={{ duration: 1 }}
@@ -30,19 +38,29 @@ export default function Landing() {
                             Secure storage
                         </p>
                     </motion.div>
-                    <p className="text-sm sm:text-base">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing
-                        elit. Illum, ea. Debitis dolore distinctio numquam
-                        ducimus, natus omnis voluptates necessitatibus iusto ab
-                        vel quia voluptatum. Nobis ut minima voluptas eaque
-                        blanditiis ad, fuga natus nemo consectetur dolor?
-                        Tempora consectetur vero quidem.
-                    </p>
-                    <div className="h-10">
-                        <Link to="/register">
-                            <Button type="primary">Get started now</Button>
-                        </Link>
-                    </div>
+                    <motion.div
+                        animate={{ x: [500, 0], opacity: [0, 1] }}
+                        transition={{ duration: 1, delay: 0.5 }}
+                    >
+                        <p className="text-sm sm:text-base">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing
+                            elit. Illum, ea. Debitis dolore distinctio numquam
+                            ducimus, natus omnis voluptates necessitatibus iusto
+                            ab vel quia voluptatum. Nobis ut minima voluptas
+                            eaque blanditiis ad, fuga natus nemo consectetur
+                            dolor? Tempora consectetur vero quidem.
+                        </p>
+                    </motion.div>
+                    <motion.div
+                        animate={{ y: [200, 0], opacity: [0, 1] }}
+                        transition={{ duration: 0.5, delay: 1.5 }}
+                    >
+                        <div className="h-10">
+                            <Link to="/register">
+                                <Button type="primary">Get started now</Button>
+                            </Link>
+                        </div>
+                    </motion.div>
                 </div>
             </div>
         </div>
